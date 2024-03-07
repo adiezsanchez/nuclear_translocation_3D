971e6709481771a05de9b2f38052ff6f79c4801a
adiezsanchez <adiez.biotech@gmail.com>
2024-03-07 23:02:46 +0100

    Refactor to loop over all folders

--- utils.py
+++ utils.py
@@ -100,6 +100,64 @@ def make_isotropic(image, scaling_x_um, scaling_y_um, scaling_z_um):
 
     return image_resampled
 
+def extract_nuclei_labels(ch00_stack, 
+                          scaling_x_um = 0.342, 
+                          scaling_y_um = 0.342, 
+                          scaling_z_um = 0.663, 
+                          top_hat_radius = 5, 
+                          gaussian_sigma = 2, 
+                          voronoi_otsu_spot_sigma = 10,
+                          voronoi_otsu_outline_sigma = 1,
+                          closing_labels_radius = 5,
+                          erosion_labels_radius = 3):
+    """Takes a nuclei stack and analysis parameters as inputs and outputs a .cle OCLarray as output containing the post-processed nuclei labels"""
+    
+    # Rescale the ch00_stack (nuclei) and ch01_stack to make data isotropic in order to apply voronoi_otsu labeling afterwards
+    nuclei_resampled = make_isotropic(ch00_stack, scaling_x_um, scaling_y_um, scaling_z_um)
+
+    # Remove background with a top_hat_filter
+    background_subtracted = cle.top_hat_box(nuclei_resampled, radius_x=top_hat_radius, radius_y=top_hat_radius, radius_z=top_hat_radius)
+
+    # Apply gaussian blur to prevent the formation of holes upon labeling
+    post_gaussian = cle.gaussian_blur(background_subtracted, sigma_x=gaussian_sigma, sigma_y=gaussian_sigma, sigma_z=gaussian_sigma)
+
+    # Voronoi-Otsu labeling
+    segmented = cle.voronoi_otsu_labeling(post_gaussian, spot_sigma=voronoi_otsu_spot_sigma, outline_sigma=voronoi_otsu_outline_sigma)
+
+    # Close holes in labels to avoid false emtpy volumes within the nuclei
+    closed_labels = cle.closing_labels(segmented, radius=closing_labels_radius)
+
+    # Erode the labels to avoid quantifying protein of interest spots in close proximity but not inside the nucleus
+    eroded_labels = cle.erode_labels(closed_labels, radius=erosion_labels_radius)
+
+    # In order to remove labels on edges I have to remove the labels from the first and last z-planes
+    # Otherwise upon applying remove_labels_on_edges (skimage.clear_border) the nuclei touching "up" and "down" will be removed
+
+    # Pull eroded_labels from GPU to manipulate it as a np.array
+    eroded_labels_np = cle.pull(eroded_labels)
+
+    # Set the first and last slices to 0
+    eroded_labels_np[0, :, :] = 0
+    eroded_labels_np[-1, :, :] = 0
+
+    # Once the top and bottom slices are empty (no labels) we can apply remove on edges
+    remove_border_labels = nsbatwm.remove_labels_on_edges(eroded_labels_np)
+    
+    return remove_border_labels
+
+def measure_intensity(nuclei_labels, marker_resampled):
+    """Takes the resampled (isotropic) nuclei labels and marker_resampled (intensity image) as inputs and outputs a dataframe with per nucleus stats"""
+    # Transform cle. OCLarrays into np.arrays for further processing using skimage.regionprops
+    nuclei_labels_np = cle.pull(nuclei_labels)
+    marker_resampled_np = cle.pull(marker_resampled)
+
+    # Extract regionprops from labels not touching the borders
+    props = regionprops_table(label_image=nuclei_labels_np, intensity_image=marker_resampled_np, properties=["label", "intensity_mean", "intensity_max", "centroid", "area_filled"])
+
+    # Construct a dataframe
+    df = pd.DataFrame(props)
+
+    return df
 
 def save_stacks(images_per_position, output_dir="./output/processed_stacks"):
     """Takes a images_per_position from read_images as input, stacks them on a per channel basis and saves the resulting images on a per position basis"""
